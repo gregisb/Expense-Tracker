f63888dfc75dda4c5d6b8ae83a2a6dfef5823145
Guilherme Regis <guilhermeb.regis@gmail.com>
2021-10-05 22:54:07 -0300

    created data template, types

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import * as C from './App.styles'
+import * as C from './App.styles';
+
+import {Item} from './types/Item';
+import {Category} from './types/Category';
+
+import { items } from './data/items';
+import { categories } from './data/categories';
+
+import { getCurrentMonth } from './helpers/dateFilter'
 
 const App = () => {
+  const [list, setLists] = useState(items);
+  const [currentMonth, setCurrentMotnh] = useState(getCurrentMonth());
+
+
   return (
     <C.Container>
       <C.Header>
         <C.HeaderText>Sitemas Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-          ...
+          {/* Area de informaçoes */}
+
+          {/* Area de inserção das informções  */}
+
+          {/* Tabela de itens  */}
         </C.Body>
     </C.Container>
   );
