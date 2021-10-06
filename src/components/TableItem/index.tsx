2a897f25bc8eba56668adfc79064ab3962ba609a
Guilherme Regis <guilhermeb.regis@gmail.com>
2021-10-06 11:28:18 -0300

    add dynamic styles to components elements

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -1,6 +1,10 @@
 import * as C from './styles';
 import { Item } from '../../types/Item';
 
+import { categories } from '../../data/categories'
+ 
+import { formatDate } from '../../helpers/dateFilter'
+
 type Props = {
     item: Item
 }
@@ -9,10 +13,18 @@ type Props = {
 export const TableItem = ({item}: Props) => {
     return (
         <C.TableLine>
-            <C.TableCollumn>...</C.TableCollumn>
-            <C.TableCollumn>{item.category}</C.TableCollumn>
+            <C.TableCollumn>{formatDate(item.date)}</C.TableCollumn>
+            <C.TableCollumn>
+                <C.Category color={categories[item.category].color}>
+                    {categories[item.category].title}
+                </C.Category>
+            </C.TableCollumn>
             <C.TableCollumn>{item.title}</C.TableCollumn>
-            <C.TableCollumn>R$ {item.value}</C.TableCollumn>
+            <C.TableCollumn>
+                <C.Value color={categories[item.category].expense ? 'red' : 'green'}>
+                    R$ {item.value}
+                </C.Value>
+            </C.TableCollumn>
         </C.TableLine>
     );
 }
